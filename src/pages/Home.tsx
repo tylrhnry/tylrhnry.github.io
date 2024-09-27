edf4bc11a72f98df539ce76d54131efc852a8b5a
user3 <user3@email.com>
2024-09-27 13:29:10 -0700

    Fixed Intro message and spacing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,8 @@ const Home: FC<any> = (): ReactElement => {
         display: "flex",
         flexDirection: { xs: "column", md: "row"},
         // margin: "0.5rem",
-        alignItems: "center",
+        // alignItems: "center",
+        alignItems: "start",
       }}>
 
         <Container disableGutters sx={{ flex: 1, margin: {xs: "0.5rem", md: "0"}, }}>
@@ -46,22 +47,29 @@ const Home: FC<any> = (): ReactElement => {
           display: "flex",
           flexDirection: "column",
           paddingLeft: {xs: "0", md: "0.5rem"},
+          padding: {xs: "1.5rem"},
+          alignItems: {xs: "center", md: "start"},
         }}>
           <Typography variant="h3" sx={{
             display: {md: "none"},
             textAlign: "center",
           }}>Home</Typography>
           <Typography sx={{ 
-            paddingTop: {xs: "1rem", md: "2rem"},
+            paddingTop: {xs: "1rem", md: "0rem"},
+            textIndent: "1.5rem",
           }}>
-            Hello! I'm Tyler Henry, and I'm passionate about functional software.<br/>
-            I love software that interacts with the real world.<br/>
-            <br/>
-            Consistently trying to learn new things has introduced me to some of the best tools.<br/>
-            Rust, Nix, Neovim, SDR, textbooks<br/>
-            Hardware integration. (radios, cameras, drones, sensors)<br/>
-            I have deployed many lora nodes, including a remote, solar-powered repeater.<br/>
-            I am currently bringing a VTOL design to CAD.<br/>
+            Hello! I’m Tyler Henry, a passionate software developer dedicated to creating functional and performant software that bridges the digital and physical worlds. My commitment to constantly stretch myself to learn new things has led me to explore an array of powerful tools and technologies, including Rust, Nix, and software-defined radio.<br/>
+          </Typography>
+          <Typography sx={{ 
+            textIndent: "1.5rem",
+          }}>
+            I'm currently in my last semester of my Bachelor's degree in Computer Science and am in the depths of writing a compiler in Rust and developing computer vision software to identify human behaviors. For my job as a Computer Science Intern at the Airforce, I am the primary developer for a Ground Penetrating Radar research project aimed at determining the efficacy of using software-defined radio as radar for bomb detection. In my personal time, I'm bringing a VTOL drone design from paper to CAD and setting up a number of mesh-networking LoRa radios and repeaters for emergency communication between my family, friends, and neighborhood.<br/>
+          </Typography>
+          <Typography sx={{ 
+            textIndent: "1.5rem",
+            paddingBottom: {sx: "1rem", md: "0.5rem"},
+          }}>
+            I’m very optimistic and enthusiastic about the impactful possibilities of well-designed software, and I intend to be at the forefront of implementing solutions that make it easier for less tech-savvy individuals to take advantage of these innovations.
           </Typography>
         </Container>
       </Container>
@@ -74,7 +82,7 @@ const Home: FC<any> = (): ReactElement => {
         display: "flex",
         flexDirection: { xs: "column", md: "row"},
         alignItems: {xs: "center", md: "start"},
-        paddingBottom: "0.5rem",
+        paddingBottom: "1.5rem",
       }}>
 
         <Container disableGutters sx={{ flex: 1, margin: {xs: "0.5rem", md: "0"}, }}>
