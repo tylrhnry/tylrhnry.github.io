1b6217b973f0f82c5ec756d8fe445b73e0800aaa
user3 <user3@email.com>
2025-07-03 11:35:06 -0700

    Removed unfinished contact page

--- src/routes.ts
+++ src/routes.ts
@@ -2,5 +2,5 @@ export const routes = [
   { key: 'home', title: 'Home', path: '/home', enabled: true },
   { key: 'experience', title: 'Experience', path: '/experience', enabled: true },
   { key: 'education', title: 'Education', path: '/education', enabled: true },
-  { key: 'contact', title: 'Contact', path: '/contact', enabled: false },
+  // { key: 'contact', title: 'Contact', path: '/contact', enabled: true },
 ];
